a3acd53d2209e9092945e18fb3c95df91217de08
Diana Muster <musterdiana@gmail.com>
2021-11-23 20:15:23 +0100

    Formular-Design erarbeitet

--- Gehaltsberechnung/Form1.Designer.cs
+++ Gehaltsberechnung/Form1.Designer.cs
@@ -29,20 +29,67 @@ namespace Gehaltsberechnung
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LBL = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Brutto";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Berechnen";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LBL
+            // 
+            this.LBL.AutoSize = true;
+            this.LBL.Location = new System.Drawing.Point(74, 251);
+            this.LBL.Name = "LBL";
+            this.LBL.Size = new System.Drawing.Size(0, 20);
+            this.LBL.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1373, 790);
+            this.ClientSize = new System.Drawing.Size(486, 459);
+            this.Controls.Add(this.LBL);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LBL;
     }
 }
 
