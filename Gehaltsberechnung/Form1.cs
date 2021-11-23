7d22fd52552faeb39b1fa8ab53089ddc9251ad95
Diana Muster <musterdiana@gmail.com>
2021-11-23 21:36:36 +0100

    Errormeldung

--- Gehaltsberechnung/Form1.cs
+++ Gehaltsberechnung/Form1.cs
@@ -21,37 +21,50 @@ namespace Gehaltsberechnung
 
         private void btn_Click(object sender, EventArgs e)
         {
-            //Textbrutto, LblAnzeige
-            //sv = brutto * 27.68 /100
-            //bemessungsgrundlage = brutto - sv
-            // if bg > 915 mit szeuer / <= 915 keine steuer
-            // netto bg - steuer
+            try
+            {
 
-            brutto = Convert.ToDouble(TxtBrutto.Text);
-            sv = brutto * 27.68 / 100;
-            bg = brutto - sv;
-            if(bg > 915)
-            {
-                steuer = bg * 25 / 100;
-            } else
-            {
-                steuer = 0;
+
+
+
+
+                //Textbrutto, LblAnzeige
+                //sv = brutto * 27.68 /100
+                //bemessungsgrundlage = brutto - sv
+                // if bg > 915 mit szeuer / <= 915 keine steuer
+                // netto bg - steuer
+
+                brutto = Convert.ToDouble(TxtBrutto.Text);
+                sv = brutto * 27.68 / 100;
+                bg = brutto - sv;
+                if (bg > 915)
+                {
+                    steuer = bg * 25 / 100;
+                }
+                else
+                {
+                    steuer = 0;
+                }
+
+                netto = bg - steuer;
+
+                /*
+                 * Brutto: 0
+                 * SV: 0
+                 * Steuer: 0
+                 * Netto: 0
+                 *  */
+                LblAnzeige.Text = "Brutto: " + string.Format("{0:C2}",brutto) + "\n"
+                    + "SV: " + string.Format("{0:C2}", sv) + "\n"
+                    + "Steuer: " + string.Format("{0:C2}", steuer) + "\n"
+                    + "Netto: " + string.Format("{0:C2}", netto);
+
+
             }
-
-            netto = bg - steuer;
-
-            /*
-             * Brutto: 0
-             * SV: 0
-             * Steuer: 0
-             * Netto: 0
-             *  */
-            LblAnzeige.Text = "Brutto: " + brutto + "\n"
-                + "SV: " + sv + "\n" 
-                + "Steuer: " + steuer + "\n"
-                + "Netto: " + netto;
-                
-
+            catch (Exception ex)
+            {
+                LblAnzeige.Text = "Fehler: " + ex;
+            }
         }
-    }
+}
 }
